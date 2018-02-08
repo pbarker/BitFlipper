637e7fbcba7c486873312dcc237e498e7c52ba03
Joy Chopra <joy.chopra2@gmail.com>
2018-02-08 19:14:38 +0530

    Update BitFlipper_env.py

--- gym_BitFlipper/envs/BitFlipper_env.py
+++ gym_BitFlipper/envs/BitFlipper_env.py
@@ -20,7 +20,7 @@ class BitFlipperEnv(gym.Env):
     self.goal = self.observation_space.sample()
     self.state = self.initial_state
     self.envstepcount = 0
-    self.reward_max = -np.sum(np.bitwise_xor(env.initial_state,env.goal))+1
+    self.reward_max = -np.sum(np.bitwise_xor(self.initial_state,self.goal))+1
     if(np.array_equal(self.goal,self.initial_state)):
        self.reward_max = -1
     
