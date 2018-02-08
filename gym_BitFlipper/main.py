b57aebbb0bdd2c58eef075d930eab96f14913677
Joy Chopra <joy.chopra2@gmail.com>
2018-02-08 13:03:57 +0530

    implemented callback

--- gym_BitFlipper/main.py
+++ gym_BitFlipper/main.py
@@ -1,13 +1,17 @@
 import gym
 from baselines import deepq
 import gym_BitFlipper
-
+import numpy as np
 from gym.envs.registration import register 
 
+reward_max=0
 def callback(lcl, _glb):  ##  make changes
   
     # stop training if reward exceeds 199
-    is_solved = lcl['t'] > 100 and sum(lcl['episode_rewards'][-101:-1]) / 100 >= 199
+    #is_solved = lcl['t'] > 100 and sum(lcl['episode_rewards'][-101:-1]) / 100 >= 199
+    reward_dist = 0.1
+    reward_max = -np.sum(np.bitwise_xor(lcl['env'].initial_state,lcl['env'].goal))+1
+    is_solved = len(lcl['episode_rewards']) > 100 and sum((lcl['episode_rewards'])[-101:-1])/100 >= (reward_max - reward_dist)
     return is_solved
   
 def main(n=10,space_seed=0):
@@ -17,9 +21,13 @@ def main(n=10,space_seed=0):
     register(id=id,entry_point='gym_BitFlipper.envs:BitFlipperEnv',kwargs = {"space_seed":space_seed,"n":n})
   except :
     print("Environment with id = "+id+" already registered.Continuing with that environment.")
+  
   env=gym.make(id)
+  reward_max = -np.sum(np.bitwise_xor(env.initial_state,env.goal))+1
+  
   print("Initial State: "+str((env.initial_state).T))
   print("Goal State: "+str((env.goal).T))
+  print("Max_reward: "+str(reward_max))
   # learning agent
   a=deepq.models.mlp([256])
   act = deepq.learn(env,q_func=a,lr=1e-4,max_timesteps=1000000,buffer_size=100000,exploration_fraction=0.02,
@@ -29,6 +37,3 @@ def main(n=10,space_seed=0):
   print("Saving model to bitflip.pkl")
   act.save("bitflip.pkl")
   
-  
-if __name__=='__main__':
-  main()
