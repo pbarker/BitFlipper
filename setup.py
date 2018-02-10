f61f0c5979569406cef255ecb5269464a1cc8480
Joy Chopra <joy.chopra2@gmail.com>
2018-02-10 10:03:40 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import setup
 
 setup(name='gym_BitFlipper',
       version='0.0.1',
-      install_requires=['gym>=0.2.3','baselines','custom_baselines'] 
+      install_requires=['gym>=0.2.3','baselines','custom_baselines'], 
+      dependency_links = ['https://github.com/JoyChopra1298/baselines.git#egg=custom_baselines']
 )  
