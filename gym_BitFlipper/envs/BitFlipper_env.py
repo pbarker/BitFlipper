9d62159ed125bfdb11afa32e69ab6e5a6952fe10
Joy Chopra <joy.chopra2@gmail.com>
2018-02-10 22:41:40 +0530

    Update BitFlipper_env.py

--- gym_BitFlipper/envs/BitFlipper_env.py
+++ gym_BitFlipper/envs/BitFlipper_env.py
@@ -5,7 +5,8 @@ from gym import spaces
 class BitFlipperEnv(gym.Env):
   '''Bit Flipping environment
       The state space is binary strings of length n.
-      The action space is an index i from {0,1...n-1} which represents the agent flipping ith bit of the environment 
+      The action space is an index i from {0,1...n} which represents the agent flipping ith bit of the environment.
+      Action = n means agent stays in the same state.
       Given an initial state the agent has to reach a goal state.
       Reward: Only goal state has reward 0,rest all states have reward -1
   '''
@@ -28,7 +29,7 @@ class BitFlipperEnv(gym.Env):
     '''
      accepts action and returns obs,reward, b_flag(episode start), info dict(optional)
     '''
-    if(self.action_space.contains(action):
+    if(self.action_space.contains(action)):
       if(!action==self.n)
         self.state = self.bitflip(action)  ## computes s_t1
       reward = self.calculate_reward()
