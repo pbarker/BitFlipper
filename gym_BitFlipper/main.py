71ca1c7b30c553123b7ee71e1d44ca677c67ba4b
Joy Chopra <joy.chopra2@gmail.com>
2018-02-10 22:13:03 +0530

    Update main.py

--- gym_BitFlipper/main.py
+++ gym_BitFlipper/main.py
@@ -11,7 +11,7 @@ def callback(lcl, _glb):
     #for deepq training
     #stop training when mean reward for last 100 episodes <= (reward_max - reward_dist)
     reward_dist = 0.1
-    is_solved = lcl['saved_mean_reward']!=None && lcl['saved_mean_reward']<=(lcl['env'].reward_max - reward_dist)
+    is_solved = (lcl['saved_mean_reward']!=None) && (lcl['saved_mean_reward']<=(lcl['env'].reward_max - reward_dist))
     return is_solved
 
 def make_env(n=10,space_seed=0):
